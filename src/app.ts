f0ec808467116b29f64d516af2aa509110abddef
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2019-07-31 22:04:25 +0300

    11. Destructuring Objects

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log('Ahoy Piper!');
+const pizzas = [
+  {name: "Pepperoni", toppings: ['peperoni']}
+]
+
+const mappedPizzas = pizzas.map( (pizza) => {
+  return pizza.name.toUpperCase()
+})
+
+console.log(mappedPizzas);
